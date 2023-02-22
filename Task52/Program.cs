00a5e8bcbe0da579ca314600c68db0c63c9dd408
AleksKolychev <aleksushome@gmail.com>
2023-02-22 09:41:34 +0500

    Add Task52

---
+++ Task52/Program.cs
@@ -0,0 +1,43 @@
+﻿/*Задайте двумерный массив из целых чисел. 
+Найдите среднее арифметическое элементов в каждом столбце.*/
+
+
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 21);
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void ReleaseMatrix(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        double result = 0;
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            
+            result = result + matrix[i, j];
+            Console.WriteLine($"Среднее арифметическое столбца {j + 1} равно {result / matrix.GetLength(0)} ");
+    }
+}    
+            
+Console.Clear();
+Console.Write("Введите размер массива: ");
+int[] size = Console.ReadLine()!.Split().Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[size[0], size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+ReleaseMatrix(matrix);
